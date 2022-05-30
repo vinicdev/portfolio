023ea5961fbc717b1123ec0d5d093e39cbaf5b29
Vinicius Cardoso <vinic8670@gmail.com>
2022-05-30 13:23:30 -0300

    Adicionando textos em Skill e Correção CSS

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,41 +1,41 @@
 
 function skill1() {
-    let txt = "HTML é fera"
+    let txt = "HTML é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores."
     document.getElementById("descriptionTech").innerHTML = txt
 }
 
 function skill2() {
-    let txt = "CSS é fera"
+    let txt = "CSS é a sigla para Cascading Style Sheets, ou seja, Folhas de Estilo em Cascatas. É uma maneira de dar estilo ao código criado por linguagens como HTML, XML ou XHTML, por exemplo."
     document.getElementById("descriptionTech").innerHTML = txt
 }
 
 function skill3() {
-    let txt = "JS é fera"
+    let txt = "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web."
     document.getElementById("descriptionTech").innerHTML = txt
 }
 
 function skill4() {
-    let txt = "REACT é fera"
+    let txt = "O React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web. "
     document.getElementById("descriptionTech").innerHTML = txt
 }
 
 function skill5() {
-    let txt = "BootStrap é fera"
+    let txt = "Bootstrap é um framework web com código-fonte aberto para desenvolvimento de componentes de interface e front-end para sites e aplicações web usando HTML, CSS e JavaScript, baseado em modelos de design para a tipografia, melhorando a experiência do usuário em um site amigável e responsivo."
     document.getElementById("descriptionTech").innerHTML = txt
 }
 
 function skill6() {
-    let txt = "GitHub é fera"
+    let txt = "GitHub é uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. "
     document.getElementById("descriptionTech").innerHTML = txt
 }
 
 function skill7() {
-    let txt = "Figma é fera"
+    let txt = "Figma é um editor gráfico de vetor e prototipagem de projetos de design baseado principalmente no navegador web."
     document.getElementById("descriptionTech").innerHTML = txt
 }
 
-function skill7() {
-    let txt = "PHP é fera"
+function skill8() {
+    let txt = "PHP é uma linguagem interpretada livre, usada originalmente apenas para o desenvolvimento de aplicações presentes e atuantes no lado do servidor, capazes de gerar conteúdo dinâmico na World Wide Web."
     document.getElementById("descriptionTech").innerHTML = txt
 }
 
