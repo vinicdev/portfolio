73afbc52909504f616cb4a4e732262020f944f89
Vinicius Cardoso <vinic8670@gmail.com>
2025-01-25 16:56:02 -0300

    fix: bug image

--- app/components/IconTech.tsx
+++ app/components/IconTech.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 interface iconTechProps {
   pathIcon: string;
 }
@@ -9,9 +7,7 @@ export default function IconTech({ pathIcon }: iconTechProps) {
     <div className="w-[80px] h-[80px] shadow bg-black-light flex items-center justify-center rounded-full">
       <div
         className={`w-[40px] h-[40px] bg-no-repeat bg-cover flex items-center justify-center ${pathIcon}`}
-      >
-        {/* <Image width={40} height={40} src={"/icon_angular.svg"} alt="teste" /> */}
-      </div>
+      ></div>
     </div>
   );
 }
